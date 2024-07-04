4f9a0c42060122578381b32a6cf2188e6a982579
Alex Sifuna <aleksifuna@gmail.com>
2024-07-04 16:03:03 +0300

    added route for drivers to post passanger review

--- api/v1/views/reviews_view.py
+++ api/v1/views/reviews_view.py
@@ -46,3 +46,37 @@ def post_driver_review(ride_id):
     driver.reviews.append(review)
     driver.save()
     return (driver.todict()), 201
+
+
+@app_views.route(
+    '/rides/<ride_id>/reviews/<user_id>',
+    methods=['POST'],
+    strict_slashes=False
+    )
+@jwt_required()
+def post_passenger_review(ride_id, user_id):
+    """
+    Handles posting of pasangers review
+    """
+    ride = Ride.objects(id=ride_id).first()
+    if not ride:
+        return jsonify({'error': 'Ride not found'})
+    if user_id not in ride.booked_seats:
+        return jsonify('Cannot perfom this action'), 401
+    driver_id = get_jwt_identity()
+    passenger = User.objects(id=user_id).first()
+    if not passenger:
+        return jsonify({'error': 'Passenger not found'})
+    data = request.json
+    required = ['rating', 'feedback']
+    for field in data:
+        if field not in required:
+            return jsonify({'error': '{field} missing'}), 400
+    review = Review()
+    for key, value in data.items():
+        setattr(review, key, value)
+    setattr(review, 'ride_id', ObjectId(ride_id))
+    setattr(review, 'user_id', ObjectId(driver_id))
+    passenger.reviews.append(review)
+    passenger.save()
+    return (passenger.todict()), 201
