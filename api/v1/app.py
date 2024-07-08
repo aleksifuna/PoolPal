376f190dfe4a31ab683c4909f01ec1f6f175c473
Alex Sifuna <aleksifuna@gmail.com>
2024-07-09 01:37:27 +0300

    made some changes on app factory

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,24 +12,38 @@ from api.v1.views import app_views
 from flask_swagger_ui import get_swaggerui_blueprint
 
 load_dotenv()
-SWAGGER_URL = "/docs"
-API_URL = "/static/swagger.json"
-swagger_ui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        "api_name": "Access API"
-    }
-)
-app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-app.config['UPLOAD_FOLDER'] = 'file_uploads/profile_pic'
-app.config['UPLOAD_EXTENSIONS'] = ['jpg', 'jpeg']
-jwt = JWTManager(app)
-cors = CORS(app, resources={r"/api/v1*": {"origins": "*"}})
-connect(db='poolpal')
-app.register_blueprint(app_views)
-app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config['UPLOAD_FOLDER'] = 'file_uploads/profile_pic'
+    app.config['UPLOAD_EXTENSIONS'] = ['jpg', 'jpeg']
+    jwt = JWTManager(app)
+    cors = CORS(app, resources={r"/api/v1*": {"origins": "*"}})
+    if config_name == 'default':
+        db_name = os.getenv('DB_NAME')
+    else:
+        db_name == 'poolpal_test'
+
+    connect(db=db_name, uuidRepresentation='standard')
+
+    SWAGGER_URL = "/docs"
+    API_URL = "/static/swagger.json"
+    swagger_ui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            "api_name": "Access API"
+        }
+    )
+
+    app.register_blueprint(app_views)
+    app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
